a22c7068ae8172b196e3d555c316a9c295ddddae
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-12 18:19:26 +0300

    add finul

--- src/components/projects/components/finul/Finul.tsx
+++ src/components/projects/components/finul/Finul.tsx
@@ -6,6 +6,7 @@ const Finul: React.FC = (): ReactElement => {
     return (
         <aside className={styles.container}>
         <img className={styles.image} src={appFinul} alt="app"/>
+            <h3 className={styles.header}>FINUL — приложение для банков с Open API</h3>
         </aside>
     );
 }
