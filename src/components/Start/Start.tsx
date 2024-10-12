47cc22510fa9259c1bf08c5660698b5e99e4ed51
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-12 13:42:49 +0300

    initial

---
+++ src/components/Start/Start.tsx
@@ -0,0 +1,36 @@
+import React, {ReactElement} from "react";
+import styles from './Start.module.scss';
+import dots from './../../assets/img/dots.svg';
+import mainImage from './../../assets/img/mainImage.svg';
+import Button from "../shared/button/Button";
+
+const Start: React.FC = (): ReactElement => {
+    return (
+        <main className={styles.container}>
+            <img className={styles.dots} src={dots} alt="dots"/>
+            <section className={styles['main']}>
+                <article className={styles['main__info']}>
+                <h1 className={styles['main__info__header']}>
+                    Разработка мобильных <br/> FinTeсh приложений</h1>
+                <p className={styles['main__info__description']}>
+                    Проектируйте и создавайте свои продукты с нами.
+                    <br/>
+                    <br/>
+                    Мы оказываем полный цикл услуг по разработке приложений в сфере финансов — весь необходимый
+                    опыт для создания решений, отвечающих бизнес-целям и потребностям пользователей.
+                    <br/>
+                    <br/>
+                    Превратите свои идеи в удобные и функциональные
+                    мобильные приложения.
+                </p>
+                    <Button caption={"Связаться с нами"}/>
+                </article>
+                <img className={styles['main__img']} src={mainImage} alt="img"/>
+            </section>
+            {/*<BaseCompanyInfo/>*/}
+        </main>
+    );
+
+}
+
+export default Start;
---
+++ src/components/shared/button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../button/Button.module.scss";
+import {ButtonPropsType} from "../../../types/button-props.type";
+
+export default class Button extends React.Component<ButtonPropsType, any> {
+    render() {
+        return (
+            <button className={styles['accept-button']}>{this.props.caption}</button>
+        );
+    }
+}
