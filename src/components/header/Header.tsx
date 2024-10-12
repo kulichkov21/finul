76c96d68fcb0251384c167cc4fcbfab5271eaa5c
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-12 14:12:47 +0300

    initial

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import styles from "./Header.module.scss"
-import logo from "../../assets/image/logo.svg"
-import {menuItems} from "./menu-items.const";
-import Button from "../shared/button/Button";
-import {Link} from "react-scroll";
 
 type HeaderProps = {
 
@@ -13,24 +9,24 @@ export default class Header extends React.Component<HeaderProps, any> {
 
     render() {
         return (
-                <header className={styles.header}>
-                    {/*<a className={styles['header__logo']} href="">*/}
-                    {/*    <img src={logo} alt="logo"/>*/}
-                    {/*    <span className={styles['header__logo__text']}>FINUL</span>*/}
-                    {/*</a>*/}
-                    {/*<nav>*/}
-                    {/*    {*/}
-                    {/*        menuItems.map((item) => {*/}
-                    {/*            return <a className={styles['header__link']}>*/}
-                    {/*                <Link delay={100} smooth={true} to={item.link}>{item.caption}</Link>*/}
-                    {/*                </a>*/}
-                    {/*        })*/}
-                    {/*    }*/}
-                    {/*</nav>*/}
-                    {/*<div className={styles['header__controls']}>*/}
-                    {/*    <Button caption={'Связаться с нами'}/>*/}
-                    {/*</div>*/}
-                </header>
+            <header className={styles.header}>
+                {/*<a className={styles['header__logo']} href="">*/}
+                {/*    <img src={logo} alt="logo"/>*/}
+                {/*    <span className={styles['header__logo__text']}>FINUL</span>*/}
+                {/*</a>*/}
+                {/*<nav>*/}
+                {/*    {*/}
+                {/*        menuItems.map((item) => {*/}
+                {/*            return <a className={styles['header__link']}>*/}
+                {/*                <Link delay={100} smooth={true} to={item.link}>{item.caption}</Link>*/}
+                {/*                </a>*/}
+                {/*        })*/}
+                {/*    }*/}
+                {/*</nav>*/}
+                {/*<div className={styles['header__controls']}>*/}
+                {/*    <Button caption={'Связаться с нами'}/>*/}
+                {/*</div>*/}
+            </header>
         );
     }
 
