f297c4f18e4761325c30b49f12088aea0634666b
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-12 13:42:25 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Start from "./components/Start/Start";
+
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <Start/>
+            </div>
+        );
+    }
+}
+
