1f3c8a9735b1165f7dd03b87fd2cdc9dd8df1744
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-13 14:11:17 +0300

    new features

--- src/components/get-consultation/GetConsultation.tsx
+++ src/components/get-consultation/GetConsultation.tsx
@@ -3,14 +3,16 @@ import styles from './GetConsultation.module.scss';
 import dots from "../../assets/img/dots2.svg";
 
 export default class GetConsultation extends React.Component<any, any> {
-
     render() {
         return (
             <section className={styles.container}>
                 <div className={styles.content}>
                     <img className={styles.dots} src={dots} alt="dots"/>
                     <h2 className={styles.header}>Получить консультацию</h2>
-                    <div >
+                    <div className={styles['get-consultation-container']}>
+                        <form action="">
+                            <input placeholder="Ваше имя" className={styles['base-input']} type="text"/>
+                        </form>
                     </div>
                 </div>
             </section>
