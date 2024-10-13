2c3dac508286e861154ffe3455704f1416bdebec
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2024-10-13 13:37:56 +0300

    new features

--- src/components/get-consultation/GetConsultation.tsx
+++ src/components/get-consultation/GetConsultation.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from './GetConsultation.module.scss';
+import dots from "../../assets/img/dots2.svg";
 
 export default class GetConsultation extends React.Component<any, any> {
 
     render() {
         return (
-            <section className={styles.container}>cccccccccccc</section>
+            <section className={styles.container}>
+                <div className={styles.content}>
+                    <img className={styles.dots} src={dots} alt="dots"/>
+                    <h2 className={styles.header}>Получить консультацию</h2>
+                    <div >
+                    </div>
+                </div>
+            </section>
         );
     }
 }
